f5cd37e69b433ab5ef23755be70fd75ced370a2f
unknown <zhoudiaofei@proton.me>
2026-03-31 15:18:59 +0800

    update files

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLAlchemy 声明式基类，所有模型继承自此 minor comment refresh
 Base = declarative_base()
 
-# 统一导入模型，便于后续 from models import Task 等方式使用
+# 统一导入模型，便于后续 from models import Task 等方式使用 minor comment refresh
 from .task import Task
 
 __all__ = [
